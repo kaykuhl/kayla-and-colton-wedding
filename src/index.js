150692c47df6fa900444e6e08fbe0b2b5dea5943
kaykuhl <kakoreo4@gmail.com>
2023-01-12 19:46:35 -0600

    fixed routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
-import NoPage from "./pages/NoPage";
 
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/kayla-and-colton-wedding" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
