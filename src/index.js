0288c0366f09f6d0d9ea67a0d74fef09bef94628
kaykuhl <kakoreo4@gmail.com>
2023-01-12 21:03:01 -0600

    homepage fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/kayla-and-colton-wedding" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
